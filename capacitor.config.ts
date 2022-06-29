8d7a4367a1072e799e85322a5d11ac2280a8ce6f
Derrick Jayasekar <64017361+Derry-Jay@users.noreply.github.com>
2022-06-30 00:31:09 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'set_in_hand',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
